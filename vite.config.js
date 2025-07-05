00c881612788670d1ee02e7be99539ca1006a203
Jiamu Sun <barroit@linux.com>
2025-07-06 03:05:41 +0900

    page: drop commit component
    Signed-off-by: Jiamu Sun <barroit@linux.com>

--- vite.config.js
+++ vite.config.js
@@ -5,41 +5,7 @@
 
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
-import { execSync } from 'node:child_process'
-import { readFileSync } from 'node:fs'
 import { defineConfig } from 'vite'
-
-const package_dict = JSON.parse(readFileSync('package.json', 'utf-8'))
-
-function exec(cmd)
-{
-	return execSync(cmd).toString().trim()
-}
-
-function last_commit()
-{
-	const hash = exec('git log -1 --format=%h')
-
-	return  JSON.stringify(hash)
-}
-
-function remote_url()
-{
-	const upstream = exec('git rev-parse \
-			       --abbrev-ref --symbolic-full-name @{u}')
-
-	const remote_col = upstream.split('/')
-	const remote = remote_col[0]
-
-	const url = exec(`git remote get-url ${remote}`)
-
-	return  JSON.stringify(url)
-}
-
-function homepage()
-{
-	return JSON.stringify(package_dict.homepage)
-}
 
 export default defineConfig({
 	plugins: [
@@ -49,10 +15,5 @@ export default defineConfig({
 	publicDir: 'assets',
 	build: {
 		outDir: 'build',
-	},
-	define: {
-		__LAST_COMMIT__: last_commit(),
-		__REMOTE_URL__: remote_url(),
-		__HOMEPAGE__: homepage(),
 	},
 })
